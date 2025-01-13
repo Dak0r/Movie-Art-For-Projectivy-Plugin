deb366bccdad91499b66af4b3714b53c6f2bd277
Daniel Korgel <contact@danielkorgel.com>
2025-01-13 22:01:23 +0100

    Improved Settings Ui

--- cinema-glow/src/main/java/com/danielkorgel/projectivy/plugin/cinemaglow/SettingsFragment.kt
+++ cinema-glow/src/main/java/com/danielkorgel/projectivy/plugin/cinemaglow/SettingsFragment.kt
@@ -1,7 +1,10 @@
 package com.danielkorgel.projectivy.plugin.cinemaglow
 
 
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.content.res.AppCompatResources
@@ -10,25 +13,41 @@ import androidx.leanback.widget.GuidanceStylist.Guidance
 import androidx.leanback.widget.GuidedAction
 import java.io.File
 
-class SettingsFragment: GuidedStepSupportFragment() {
+class SettingsFragment : GuidedStepSupportFragment() {
 
     override fun onCreateGuidance(savedInstanceState: Bundle?): Guidance {
         return Guidance(
             getString(R.string.plugin_name),
             getString(R.string.plugin_description),
             getString(R.string.settings),
-            AppCompatResources.getDrawable(requireActivity(), R.drawable.ic_plugin)
+            AppCompatResources.getDrawable(requireActivity(), R.drawable.ic_banner_drawable)
         )
     }
 
     override fun onCreateActions(actions: MutableList<GuidedAction>, savedInstanceState: Bundle?) {
-        val action = GuidedAction.Builder(context)
+        val actionClearCache = GuidedAction.Builder(context)
             .id(ACTION_ID_CLEAR_CACHE)
             .title(R.string.setting_clear_cache_title)
             .description(R.string.setting_clear_cache_description)
             .descriptionEditable(false)
             .build()
-        actions.add(action)
+        actions.add(actionClearCache)
+
+        val actionGetProjectIvy = GuidedAction.Builder(context)
+            .id(ACTION_ID_GET_PROJECTIVY)
+            .title(R.string.setting_projectivy_title)
+            .description(R.string.setting_projectivy_description)
+            .descriptionEditable(false)
+            .build()
+        actions.add(actionGetProjectIvy)
+
+        val actionAuthor = GuidedAction.Builder(context)
+            .id(ACTION_ID_AUTHOR)
+            .title(R.string.setting_danielkorgel_title)
+            .description(R.string.setting_danielkorgel_description)
+            .descriptionEditable(false)
+            .build()
+        actions.add(actionAuthor)
     }
 
     override fun onGuidedActionClicked(action: GuidedAction) {
@@ -42,11 +61,28 @@ class SettingsFragment: GuidedStepSupportFragment() {
                 ).show()
                 println("Cache cleared!")
             }
+
+            ACTION_ID_GET_PROJECTIVY -> {
+                val packageName = "com.spocky.projengmenu"
+                try {
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$packageName")))
+                } catch (e: ActivityNotFoundException) {
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=$packageName")))
+                }
+                println("Projectivy Playstore opened!")
+            }
+
+            ACTION_ID_AUTHOR -> {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.danielkorgel.com")))
+                println("Website Opened!")
+            }
         }
     }
 
     companion object {
         private const val ACTION_ID_CLEAR_CACHE = 1L
+        private const val ACTION_ID_GET_PROJECTIVY = 2L
+        private const val ACTION_ID_AUTHOR = 3L
     }
 
     fun clearExternalCache(context: Context) {
